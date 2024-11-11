0d3b5fe11c524cd8cfe83206272cacd520198a80
Camilo Baggio <baggiocamilo@gmail.com>
2024-11-11 14:57:23 -0300

    properties,game, IRM

--- src/main/java/com/devproject/DSlist/DSlistApplication.java
+++ src/main/java/com/devproject/DSlist/DSlistApplication.java
@@ -1,4 +1,4 @@
-package com.devproject.DSlist;
+package com.devproject.dslist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
