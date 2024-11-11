15ea91e2776ec75d7582289451705499a77f9961
Camilo Baggio <baggiocamilo@gmail.com>
2024-11-11 11:49:23 -0300

    project created

---
+++ src/test/java/com/devproject/DSlist/DSlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devproject.DSlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DSlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
